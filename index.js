a659a7383214404d2590ca3e9d9ce8917212fd4a
Michael <mkenton2020@icloud.com>
2021-07-11 22:01:26 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+
+const names = ["Jeremy", "Joy", "Johnathan"];
+const eventName = "housewarming"
+
+function writeCards(names, eventName) {
+   let messages; 
+   messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        messages[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        debugger;
+    }
+    return messages;
+}
+
+let x = writeCards(names,eventName);
+console.log(x)
+
+function countDown(n) {
+    while (n >= 0) {
+    console.log(n);
+    n-- ;}
+}
